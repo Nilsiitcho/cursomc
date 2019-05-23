8e4531489f6f7761be67aa12e509975d0c8f5b44
Nilson Neto <nyson96@hotmail.com>
2019-05-22 22:56:21 -0300

    Criação das entidades cidades e estados

--- src/main/java/br/com/estudo/estudodecaso/CursomcApplication.java
+++ src/main/java/br/com/estudo/estudodecaso/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.estudo.estudodecaso.domain.Categoria;
+import br.com.estudo.estudodecaso.domain.Cidade;
+import br.com.estudo.estudodecaso.domain.Estado;
 import br.com.estudo.estudodecaso.domain.Produto;
 import br.com.estudo.estudodecaso.repositories.CategoriaRepository;
+import br.com.estudo.estudodecaso.repositories.CidadeRepository;
+import br.com.estudo.estudodecaso.repositories.EstadoRepository;
 import br.com.estudo.estudodecaso.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,36 +21,53 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	ProdutoRepository produtoRepository;
-	
+
+	@Autowired
+	CidadeRepository cidadeRepository;
+
+	@Autowired
+	EstadoRepository estadoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressoras", 800.00);
 		Produto p3 = new Produto(null, "Mopuse", 80.00);
-		
+
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
-		
-		
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.save(Arrays.asList(est1, est2));
+		cidadeRepository.save(Arrays.asList(c1, c2, c3));
+
 	}
-	
 
 }
