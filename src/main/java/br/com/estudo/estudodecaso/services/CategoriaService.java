906294df792c3144614a674a97a94fe9661f3048
Nilson Neto <nyson96@hotmail.com>
2019-05-26 18:33:44 -0300

    Atualizando categoria com PUT e pequenos ajudstes

--- src/main/java/br/com/estudo/estudodecaso/services/CategoriaService.java
+++ src/main/java/br/com/estudo/estudodecaso/services/CategoriaService.java
@@ -12,18 +12,24 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public Categoria buscar(Integer id) {
+
+	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
-		if(obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
 		}
 		return obj;
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
+	public Categoria update(Categoria categoria) {
+		find(categoria.getId());
+		return repo.save(categoria);
+	}
+
 }
--- src/main/java/br/com/estudo/estudodecaso/services/ClienteService.java
+++ src/main/java/br/com/estudo/estudodecaso/services/ClienteService.java
@@ -13,7 +13,7 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repository;
 
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Cliente cliente = repository.findOne(id);
 		if (cliente == null) {
 			throw new ObjectNotFoundException(
